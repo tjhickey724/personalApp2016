1cb0ca95b7329655ebf4d6c3dfaf1fc0231b7290
tim <tjhickey@brandeis.edu>
2016-06-09 14:08:18 -0400

    added a delete button

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -1,18 +1,23 @@
 Template.comments.helpers({
 	commentsdata: function(){
-		return Comments.find({},{sort:{createdAt:-1}, limit:3});},
+		return Comments.find({},{sort:{createdAt:-1}, limit:30000});},
 	
 	
 })
 
 Template.comments.events({
-	"click .js-submit-comment": 
-	 function(event){
+	"click .js-submit-comment": function(event){
 	   event.preventDefault();
 	   //console.dir(event);
 	   const comment_text = $(".js-user-comment").val();
+	   if (comment_text=="") {
+	   	window.alert("you must enter a comment!");
+	   	return;
+	   }
+	   const comment_rating = $(".js-rating-comment").val();
 	   const comment_obj =
 	   {text: comment_text,
+	   	rating:comment_rating,
 	    createdAt: new Date(),
 	    createdBy: Meteor.userId(),
 	    userEmail: Meteor.user().emails[0].address};
@@ -21,5 +26,13 @@ Template.comments.events({
 	    $(".js-user-comment").val("");
 	    //Router.go('/');
 	    console.log("Did we get here????")
-	}
+	},
+});
+
+Template.commentRow.events({
+	"click .js-delete-comment": function(event){
+		console.log("clicked on the x");
+		console.dir(this);
+		Comments.remove(this.comment._id);
+	},
 })
