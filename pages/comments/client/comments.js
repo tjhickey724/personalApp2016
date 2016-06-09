4da732d580aa52e39464f20bf2ce9ef764656f25
tim <tjhickey@brandeis.edu>
2016-06-09 13:28:55 -0400

    added Comments collection

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -1,4 +1,7 @@
 Template.comments.helpers({
+	commentsdata: function(){
+		return Comments.find({},{sort:{createdAt:-1}, limit:3});},
+	
 	
 })
 
@@ -6,15 +9,17 @@ Template.comments.events({
 	"click .js-submit-comment": 
 	 function(event){
 	   event.preventDefault();
-	   console.dir(event);
+	   //console.dir(event);
 	   const comment_text = $(".js-user-comment").val();
 	   const comment_obj =
 	   {text: comment_text,
 	    createdAt: new Date(),
 	    createdBy: Meteor.userId(),
 	    userEmail: Meteor.user().emails[0].address};
-	    console.dir(comment_obj);
-	    //Router.go({{pathFor 'home'}});
+	    //console.dir(comment_obj);
+	    Comments.insert(comment_obj);
 	    $(".js-user-comment").val("");
+	    //Router.go('/');
+	    console.log("Did we get here????")
 	}
 })
